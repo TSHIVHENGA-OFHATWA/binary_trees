f0c3a873bbb6a105017a97164bb53ea0de2cd7d9
OFHATWA <tofhatwa@gmail.com>
2024-06-06 16:35:45 +0200

    Adding task 10 to 14

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_is_root - Checks if a given node is a root
  * @node: Pointer to the node to check
- * 
+ *
  * Return: 1 if node is a root, otherwise 0
  *         If node is NULL, return 0
  */
