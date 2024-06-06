4666f1d8528b676a0fb0e7458caddf8d4ad0bfe1
OFHATWA <tofhatwa@gmail.com>
2024-06-06 12:38:04 +0200

    Adding the header file containing all the prototypes

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,9 @@
-#ifndef _TREES_H_
-#define _TREES_H_
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
+
+#include <stddef.h>
+#include <stdlib.h>
+
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -9,13 +13,15 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
-};
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+} binary_tree_t;
+
+void binary_tree_print(const binary_tree_t *);
 
 typedef struct binary_tree_s binary_tree_t;
 
@@ -63,4 +69,4 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
-#endif
+#endif /* _BINARY_TREES_H_ */
